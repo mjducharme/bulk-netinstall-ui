64e95a785b7f254a2b77b619f691754dbfb2a1ce
Michael Ducharme <mducharme@gmail.com>
2021-08-26 18:30:49 -0700

    POST method

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-sm-1 my-auto">{{ $loop->iteration }}</div>
                 <div class="col-sm-1 my-auto">{{ $interface->interface_friendly_name }}</div>
                 <div class="col-sm-8 my-auto">
-                    <form style="margin-bottom: 0px; ">
+                    <form style="margin-bottom: 0px; " method="POST">
                         <div class="form-row">
                             <div class="col-3">
                                 <select class="form-control" id="selectOperation"
