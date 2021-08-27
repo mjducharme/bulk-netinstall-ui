7da4696d1a85afa1db7524cf1110e3344d4e4976
Michael Ducharme <mducharme@gmail.com>
2021-08-26 18:28:40 -0700

    Formatting fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,46 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container">
-  <div class="row">
-    <div class="col-sm-1">Slot</div>
-    <div class="col-sm-1">Interface</div>
-    <div class="col-sm-8">Current Operation</div>
-    <div class="col-sm-2">Previous Operation</div>
-  </div>
-  <hr/>
-  @foreach ($interfaces as $interface)
-  <div class="row">
-    <div class="col-sm-1 my-auto">{{ $loop->iteration }}</div>
-    <div class="col-sm-1 my-auto">{{ $interface->interface_friendly_name }}</div>
-    <div class="col-sm-8 my-auto"><form style="margin-bottom: 0px; ">
-    <div class="form-row">
-    <div class="col-3">  
-    <select class="form-control" id="selectOperation" onchange="displayDivDemo('selectCol{{ $loop->iteration }}', this)">
-      <option value="0">Reset Slot</option>
-      <option value="1">Prepare Device</option>
-      <option value="2">Apply Template</option>
-    </select>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-1">Slot</div>
+            <div class="col-sm-1">Interface</div>
+            <div class="col-sm-8">Current Operation</div>
+            <div class="col-sm-2">Previous Operation</div>
+        </div>
+        <hr />
+        @foreach ($interfaces as $interface)
+            <div class="row">
+                <div class="col-sm-1 my-auto">{{ $loop->iteration }}</div>
+                <div class="col-sm-1 my-auto">{{ $interface->interface_friendly_name }}</div>
+                <div class="col-sm-8 my-auto">
+                    <form style="margin-bottom: 0px; ">
+                        <div class="form-row">
+                            <div class="col-3">
+                                <select class="form-control" id="selectOperation"
+                                    onchange="displayDivDemo('selectCol{{ $loop->iteration }}', this)">
+                                    <option value="0">Reset Slot</option>
+                                    <option value="1">Prepare Device</option>
+                                    <option value="2">Apply Template</option>
+                                </select>
+                            </div>
+                            <div class="col" id="selectCol{{ $loop->iteration }}" style="display:none;">
+                                <select class="form-control" id="selectTemplate">
+                                    @foreach ($templates as $template)
+                                        <option>{{ $template->description }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col mt-auto">
+                                <button type="submit" class="btn btn-primary">Go</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-sm-2"><b>Op:</b> None<br /><b>Result:</b> None</div>
+            </div>
+            <hr />
+        @endforeach
     </div>
-    <div class="col" id="selectCol{{ $loop->iteration }}" style="display:none;">
-    <select class="form-control" id="selectTemplate">
-    @foreach ($templates as $template)
-      <option>{{ $template->description }} </option>
-    @endforeach  
-    </select>
-</div><div class="col mt-auto">
-<button type="submit" class="btn btn-primary">Go</button>
-</div></div></form></div>
-<div class="col-sm-2"><b>Op:</b> None<br/><b>Result:</b> None</div>
-  </div>
-  <hr/>
-  @endforeach
-</div>
 @endsection
 
 <script type="text/javascript">
-function displayDivDemo(id, elementValue) {
-      document.getElementById(id).style.display = elementValue.value == 2 ? 'block' : 'none';
-   }
+    function displayDivDemo(id, elementValue) {
+        document.getElementById(id).style.display = elementValue.value == 2 ? 'block' : 'none';
+    }
 </script>
